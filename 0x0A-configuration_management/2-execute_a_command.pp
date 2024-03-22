928e55f083926a53a482aefd3dceef40f9a2ba0c
ayoubleass <ayoubleas@gmail.com>
2024-03-22 07:29:21 -0700

    try to fix task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,8 @@
 # create a manifest that kills a process named killmenow.
 
 exec { 'pkill':
-  command => 'pkill killmenow',
+  command =>  'pkill killmenow',
+  path    =>  '/usr/bin',
+  provider=>  'shell', 
 }
 
