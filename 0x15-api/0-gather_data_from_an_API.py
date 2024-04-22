0e71c101a5965f6f29999770606a7b9cdaa05534
ayoubleass <ayoubleas@gmail.com>
2024-04-22 07:09:48 -0700

    adding task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" This script fetch data from RESTApi and displays it to the stdout"""
+
+
+import requests
+from sys import argv
+
+url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+r = requests.get(url)
+name = r.json().get('name')
+user_id = r.json().get('id')
+url = "https://jsonplaceholder.typicode.com/todos/"
+r = requests.get(url)
+
+tasks_completed = 0
+total_tasks = 0
+titles = []
+for task in r.json():
+    if int(user_id) == task.get("userId"):
+        total_tasks = total_tasks + 1
+        if task.get('completed'):
+            tasks_completed += 1
+            titles.append(task.get('title'))
+print("Employee {} is done with tasks({}/{}):".format(
+    name, tasks_completed, total_tasks)
+    )
+print("\n".join("\t" + title for title in titles))
