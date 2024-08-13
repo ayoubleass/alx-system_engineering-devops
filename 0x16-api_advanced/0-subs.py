922a31935d7aa1f12dc69f1de562689becef6209
ayoubleass <ayoubleas@gmail.com>
2024-08-13 07:20:18 -0700

    init the directory and creating 0-subs.py and README.md file

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This script has a function that
+Returns the number of subscribers for a given subreddit"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers for a given subreddit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "My-User-Agent"}
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    if r.status_code == 200:
+        return r.json().get('data').get('subscribers')
+    return 0
