48ebe0bcfced884a7c4560a4b9fe20fa743a6d5f
ayoubleass <ayoubleas@gmail.com>
2024-08-13 07:28:54 -0700

    correcting file 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,6 +2,7 @@
 """This script has a function that
 Returns the number of subscribers for a given subreddit"""
 
+
 import requests
 
 
@@ -10,6 +11,6 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "My-User-Agent"}
     r = requests.get(url, headers=headers, allow_redirects=False)
-    if r.status_code == 200:
-        return r.json().get('data').get('subscribers')
-    return 0
+    if r.status_code >= 300:
+        return 0
+    return r.json().get('data').get('subscribers')
